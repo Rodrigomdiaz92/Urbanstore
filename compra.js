8eb5afa787f12be9b07fca1e47acb33078fadf79
Rodrigomdiaz92 <rodrigomdiaz92@gmail.com>
2025-06-11 18:28:02 -0300

    pantalla de compra localstorage

--- compra.js
+++ compra.js
@@ -1,53 +1,42 @@
+function obtenerCarrito() {
+  return JSON.parse(localStorage.getItem('carrito')) || [];
+}
 
+function vaciarCarrito() {
+  localStorage.removeItem('carrito');
+  renderCarrito();
+}
 
+function renderCarrito() {
 
-
-
-
-let carritoActivo=[
-    {
-    id: 1,
-    nombre: "Zapatilla Nike Air Max",
-    categoria: "zapatillas",
-    marca: "Nike",
-    talle: [39, 40, 41, 42, 43],
-    precio: 45000,
-    stock: 12,
-    color: "Negro",
-    imagen: "../img/zapatillas/nike1.jpg"
-  },
-  {
-    id: 2,
-    nombre: "Zapatilla Adidas Ultraboost",
-    categoria: "zapatillas",
-    marca: "Adidas",
-    talle: [38, 39, 40, 41, 42],
-    precio: 50000,
-    stock: 8,
-    color: "Blanco",
-    imagen: "../img/zapatillas/addidas1.jpg"
-  },
-  ];
-
-
-  function renderCarrito(carrito) {
+  let carrito ={records: obtenerCarrito()}
+  console.log(carrito)
+  
   const contenedor = document.getElementById("carritoItems");
   const totalTexto = document.getElementById("totalCarrito");
+  if(carrito.records.length == 0){
+    contenedor.innerHTML  = `<h4>Sin productos</h4>`;
+
+  }else{
   contenedor.innerHTML = "";
 
   let total = 0;
 
-  carrito.forEach(producto => {
-    total += producto.precio;
+  carrito.records.forEach(producto => {
+    const p = producto.fields;
+    const recordId = producto.id;
+    total += p.precio;
 
     const item = document.createElement("div");
     item.classList.add("item-carrito");
 
     item.innerHTML = `
-      <img src="${producto.imagen}" alt="${producto.nombre}">
+      <a href="detalleproducto.html?id=${recordId}">
+        <img src="${p.imagen}" alt="${p.nombre}">
+      </a>
       <div>
-        <h4>${producto.nombre}</h4>
-        <p>$${producto.precio}</p>
+        <h4>${p.nombre}</h4>
+        <p>$${p.precio}</p>
       </div>
     `;
 
@@ -56,5 +45,6 @@ let carritoActivo=[
 
   totalTexto.textContent = total;
 }
+}
 
-renderCarrito(carritoActivo)
+renderCarrito()
